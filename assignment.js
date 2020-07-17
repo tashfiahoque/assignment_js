9b767fd35a3ea0dc51038d718108db2a626d8b00
Your Name <you@example.com>
2020-07-17 21:26:27 +0600

    updates

--- assignment.js
+++ assignment.js
@@ -1,4 +1,5 @@
 //feetToMile conversion function
+
 function feetToMile(feet){
     if(feet >= 0){
         let mile = feet * 0.00018939;
@@ -12,21 +13,29 @@ function feetToMile(feet){
 const result1 = feetToMile(5);
 console.log("The Result is :", result1);
 
+
 //woodCalculator function
-function woodCalculator(furniture){
-    for(var arr of furniture){                                                  // To avoid negative input
-        if(arr < 0) 
-        return "Please enter valid value";
+
+let furniture = [4, 5, 2];                                                           // Here furniture = ["chair", "table", "cot"].All values in units
+if (furniture && furniture.length){
+    function woodCalculator(furniture){
+        for(var arr of furniture){                                                  // To avoid negative input
+            if(arr < 0) 
+            return "Please enter valid value";
+        }
+        let i = 0;
+        let sum = (furniture[i] * 1) + (furniture[i+1] * 3) + (furniture[i+2] * 5);   // 1, 3, 5 are measuring units in cubic feet
+        return sum;
     }
-    let i = 0;
-    let sum = (furniture[i] * 1) + (furniture[i+1] * 3) + (furniture[i+2] * 5);   // 1, 3, 5 are measuring units in cubic feet
-    return sum;
+        var result2 = woodCalculator(furniture);
+        console.log(result2);                                                        // Result is in cubic feet
+}else{
+    console.log("Please Enter values in furniture array");
 }
-let furniture = [4, 6, 2];                                                     // Here furniture = ["chair", "table", "cot"].All values in units
-var result2 = woodCalculator(furniture);
-console.log(result2);                                                           // Result is in cubic feet
+                                                           
 
 //brickCalculator function
+
 const brickPerHeight = 1000;                                                   // Amount of brick  
 let totalBrick  = 0;
 let totalBrick1 = 0;
@@ -53,18 +62,24 @@ const result3 = brickCalculator(25);
 console.log(result3);                                                       // Result indicates amount of brick
 
 //tinyFriend function
-function tinyFriend(name){
-    for (var i = 0; i < name.length; i++) {                               // To Remove White Space
-        name[i] = name[i].trim();
-    }
-    let smallest = name[0];
-    for (let i = 1; i < name.length; i++) {
-        if (name[i].length < smallest.length) {
-            smallest = array[i];
+
+let name = [" Sad ", "Zahid", "Ayyan", "Tina", "Easha" ];
+if (name && name.length){
+    function tinyFriend(name){    
+        for (var i = 0; i < name.length; i++) {                               // To Remove White Space
+            name[i] = name[i].trim();
         }
+        let smallest = name[0];
+        for (let i = 1; i < name.length; i++) {
+            if (name[i].length < smallest.length) {
+            smallest = array[i];
+            }
+        }
+        return smallest;
     }
-    return smallest;
+        const result4 = tinyFriend(name);
+        console.log(result4);
 }
-let name = [" Sad ", "Ayyan", "Monirul", "Easha", "Tina"];
-const result4 = tinyFriend(name);
-console.log(result4);
+else{
+    console.log("Please enter valus in name array.");
+}
